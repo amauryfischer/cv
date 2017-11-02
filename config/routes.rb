190d696f670c5e3bfec7d507c2c512aa33695378
amauryfischer <amaufischer@gmail.com>
2017-11-02 17:12:35 +0800

    adding system

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/home' => "base#home"
+  get '/' => redirect("/home")
+  get '/vincentboutry' => "base#vincentboutry"
+  get '/amauryfischer' => "base#amauryfischer"
+get "*path" => redirect("/home")
 end
